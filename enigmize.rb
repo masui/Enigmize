bba43f6dd8587968a51587f0034df3bfe6529f87
Toshiyuki Masui <masui@pitecan.com>
2021-02-17 21:31:37 +0900

    ...

--- enigmize.rb
+++ enigmize.rb
@@ -28,7 +28,7 @@ end
 
 get '/*@*.ink' do
   (@name, @domain) = params['splat']
-  email = "#{@name}@#{@domain}"
+  email = "#{@name}@#{@domain}
   @email = email
   ink = ''
   timestamp = ''
@@ -73,7 +73,7 @@ end
 
 get '/*@*' do
   (@name, @domain) = params['splat']
-  @email = "#{name}@#{domain}"
+  @email = "#{@name}@#{@domain}"
   @timestamp = ''
   Mongo::Client.new(ENV['MONGODB_URI']) { |db|
     db[:users].find({ email: @email }).each { |e|
